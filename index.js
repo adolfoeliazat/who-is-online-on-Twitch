e78ffdb071107187008a2786e1972a03acc369a5
Hirokazu Hirono <lost.my.phone.8.5@gmail.com>
2017-06-10 15:24:45 +0900

    added click handler

--- index.js
+++ index.js
@@ -3,20 +3,21 @@
 // console.log(menus[1].dataset.status);
 // console.log(menus[2].dataset.status);
 // API_KEY = kkaeq46sq7dzoazhviihhcpba37y4r
-const searchText = $('#searchText').val();
-const text = 'zon';
+
 const API_KEY = 'kkaeq46sq7dzoazhviihhcpba37y4r';
-const url = `https://api.twitch.tv/kraken/users/${text}?client_id=${API_KEY}`;
-const streamUrl = `https://api.twitch.tv/kraken/streams/${text}?client_id=${API_KEY}`;
 const result = document.getElementById('result');
 const li = document.createElement('li');
 const img = document.createElement('img');
 const p1 = document.createElement('p');
 const p2 = document.createElement('p');
 const a = document.createElement('a');
+const btn = document.getElementById('btn');
 
-
-$.getJSON(url).done((data) => {
+btn.addEventListener('click', () => {
+  const searchText = $('#searchText').val();
+  const url = `https://api.twitch.tv/kraken/users/${searchText}?client_id=${API_KEY}`;
+  const streamUrl = `https://api.twitch.tv/kraken/streams/${searchText}?client_id=${API_KEY}`;
+  $.getJSON(url).done((data) => {
   $.getJSON(streamUrl).done((res) => {
    if (res.stream === null) {
         console.log('basic info: ', data);
@@ -40,6 +41,33 @@ $.getJSON(url).done((data) => {
     }
   });
 });
+});
+
+
+// $.getJSON(url).done((data) => {
+//   $.getJSON(streamUrl).done((res) => {
+//    if (res.stream === null) {
+//         console.log('basic info: ', data);
+//         p1.innerText = data.display_name;
+//         p2.innerText = 'offline now.';
+//         li.appendChild(p1);
+//         li.appendChild(p2);
+//         img.src = data.logo;
+//         li.appendChild(img);
+//         result.appendChild(li);
+//       } else {
+//         p1.innerText = data.display_name;
+//         p2.innerText = 'online now.';
+//         a.href = res.channel.url;
+//         li.appendChild(p1);
+//         li.appendChild(p2);
+//         li.appendChild(a);
+//         img.src = data.logo;
+//         li.appendChild(img);
+//         result.appendChild(li);
+//     }
+//   });
+// });
 
 // $.getJSON(url).done((data) => {
 //   console.log('basic info: ', data);
