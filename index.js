f2d6a15c23b94db968faa32e34f087bc6fbbcdf5
Hirokazu Hirono <lost.my.phone.8.5@gmail.com>
2017-06-10 15:13:37 +0900

    done so far

--- index.js
+++ index.js
@@ -2,14 +2,53 @@
 // console.log(menus[0].dataset.status);
 // console.log(menus[1].dataset.status);
 // console.log(menus[2].dataset.status);
-$(document).ready(function() {
-  const searchText = $('#searchText').val();
-  const url = 'https://api.twitch.tv/kraken/streams/freecodecamp';
-  $.getJSON(url, (data1) => {
-    if (data1.streams === null) {
-      console.log('freeCodeCamp is offline');
-    } else {
-      console.log('Online!');
+// API_KEY = kkaeq46sq7dzoazhviihhcpba37y4r
+const searchText = $('#searchText').val();
+const text = 'zon';
+const API_KEY = 'kkaeq46sq7dzoazhviihhcpba37y4r';
+const url = `https://api.twitch.tv/kraken/users/${text}?client_id=${API_KEY}`;
+const streamUrl = `https://api.twitch.tv/kraken/streams/${text}?client_id=${API_KEY}`;
+const result = document.getElementById('result');
+const li = document.createElement('li');
+const img = document.createElement('img');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+const a = document.createElement('a');
+
+
+$.getJSON(url).done((data) => {
+  $.getJSON(streamUrl).done((res) => {
+   if (res.stream === null) {
+        console.log('basic info: ', data);
+        p1.innerText = data.display_name;
+        p2.innerText = 'offline now.';
+        li.appendChild(p1);
+        li.appendChild(p2);
+        img.src = data.logo;
+        li.appendChild(img);
+        result.appendChild(li);
+      } else {
+        p1.innerText = data.display_name;
+        p2.innerText = 'online now.';
+        a.href = res.channel.url;
+        li.appendChild(p1);
+        li.appendChild(p2);
+        li.appendChild(a);
+        img.src = data.logo;
+        li.appendChild(img);
+        result.appendChild(li);
     }
   });
 });
+
+// $.getJSON(url).done((data) => {
+//   console.log('basic info: ', data);
+//   p.innerText = data.display_name;
+//   li.appendChild(p);
+//   img.src = data.logo;
+//   li.appendChild(img);
+//   result.appendChild(li);
+// });
+// $.getJSON(streamUrl).done((data) => {
+//   console.log('streaming?: ', data.stream);
+// });
